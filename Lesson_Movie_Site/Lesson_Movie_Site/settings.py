23545d6720e1b8267f4510a8ff9c208b77823d5a
mavlannnn <156051704+mavlannnn@users.noreply.github.com>
2024-05-02 14:40:24 +0600

    .

--- Lesson_Movie_Site/Lesson_Movie_Site/settings.py
+++ Lesson_Movie_Site/Lesson_Movie_Site/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.telegram',
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.github',
 ]
 
 MIDDLEWARE = [
