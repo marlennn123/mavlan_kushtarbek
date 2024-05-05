13585afd713165105ed1d018f02c6a2aa92efe9e
Kushtarbek <satarovkushtarbek05@gmail.com>
2024-05-06 00:58:58 +0600

    Changed urls.py, views.py, serializers.py

--- Lesson_Movie_Site/movie_site/urls.py
+++ Lesson_Movie_Site/movie_site/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
          name='category_detail'),
 
 
-    path('actor', ActorViewSets.as_view({'get': 'list', 'post': 'create'}),
+    path('actor/', ActorViewSets.as_view({'get': 'list', 'post': 'create'}),
          name='actor_list'),
     path('actor/<int:pk>/', ActorViewSets.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}),
          name='actor_detail'),
@@ -29,13 +29,13 @@ urlpatterns = [
          name='director'),
 
 
-    path('genre', GenreViewSets.as_view({'get': 'list', 'post': 'create'}),
+    path('genre/', GenreViewSets.as_view({'get': 'list', 'post': 'create'}),
          name='genre_list'),
     path('genre/<int:pk>/', GenreViewSets.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}),
          name='genre_detail'),
 
 
-    path('movie', MovieViewSets.as_view({'get': 'list', 'post': 'create'}),
+    path('movie/', MovieViewSets.as_view({'get': 'list', 'post': 'create'}),
          name='movie_list'),
     path('movie/<int:pk>/', MovieViewSets.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}),
          name='movie_detail'),
@@ -52,7 +52,7 @@ urlpatterns = [
          name='rating_detail'),
 
 
-    path('reviews', ReviewViewSets.as_view({'get': 'list', 'post': 'create'}),
+    path('reviews/', ReviewViewSets.as_view({'get': 'list', 'post': 'create'}),
          name='review_list'),
     path('reviews/<int:pk>/', ReviewViewSets.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}),
          name='review_detail'),
--- Lesson_Movie_Site/movie_site/views.py
+++ Lesson_Movie_Site/movie_site/views.py
@@ -6,6 +6,7 @@ from rest_framework.filters import SearchFilter
 from .serializers import *
 
 
+
 class WomenAPIListPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
